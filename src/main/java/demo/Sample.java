fa525f94ae309836acf5e2b92fd0e60472f82631
YMali <yogitamali2015@gmail.com>
2023-09-12 16:24:42 +0530

    Commit and push

--- src/main/java/demo/Sample.java
+++ src/main/java/demo/Sample.java
@@ -3,6 +3,9 @@ package demo;
 public class Sample {
 	public static void main(String[]args) {
 		System.out.println("Hi this is my first commit");
+		
+		System.out.println("yogitamali");
+		
 	}
 
 }
